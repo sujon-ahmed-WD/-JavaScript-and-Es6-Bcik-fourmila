cd9ed93bb5e2891347086754860e9b9d4db15e56
sujon0193 <sujon.mrist@gmail.com>
2024-03-07 07:32:59 +0600

    for each

---
+++ Map, ForEach, Filter And Find .js
@@ -0,0 +1,13 @@
+const prodacts = [
+  { name: "laptop", pries: 320, barand: "lenvo", color: "silver" },
+  { name: "laptop", pries: 320, barand: "lenvo", color: "silver" },
+  { name: "laptop", pries: 320, barand: "lenvo", color: "silver" },
+  { name: "laptop", pries: 320, barand: "js", color: "silver" },
+  { name: "laptop", pries: 320, barand: "hp", color: "silver" },
+  { name: "laptop", pries: 320, barand: "lenvo", color: "silver" },
+];
+// const brands = prodact.map((p) => p.name);
+// console.log(brands);
+// const eng = prodact.map((k) => k.pries);
+// console.log(eng);
+prodacts.forEach((l) => console.log(l.pries));
---
+++ praties.js
@@ -0,0 +1,11 @@
+const sujon = (s, js) => {
+  console.log(s, js);
+};
+const simbule = () => nsja;
+const dotoprarto = (a, b) => {
+  console.log(a, b);
+};
+const s = () => {};
+const yj = () => {
+  console.log("hello bangladesh ");
+};
